3e1be635337db39d1802a8c2f4d49b190e0da628
Replit user <>
2023-03-23 01:26:43 +0000

    new try

--- list.c
+++ list.c
@@ -105,7 +105,7 @@ void pushBack(List * list, void * data) {
 void pushCurrent(List * list, void * data) {
   Node *nuevoNodo = createNode(data);
   
-  if (list->head == NULL ||  list->current == NULL)
+  if (list->head == NULL)
   {
     pushFront(list, data);
   }
